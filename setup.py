dedcd3f877a000db157f9444fcf794240b8e172f
Matthew Hasselfield <mhasself@users.noreply.github.com>
2018-09-14 15:02:32 -0400

    Actually, just use Makefiles instead of re-implementing make inside setup.py...

--- setup.py
+++ setup.py
@@ -26,69 +26,14 @@ compile_opts = {
     'extra_link_args': ['-fopenmp']
     }
 
-class CythonTarget:
-    def __init__(self, c_filename, pyx_filename=None, deps=None, root=None):
-        """
-        Instantiate with the c_filename that is the output of this cython
-        compilation.  If pyx_filename is is non-trivial, pass it too.
-        If there are other dependencies that should trigger a rebuild,
-        list them in deps.  If root is not None, then all other
-        filenames are assumed to be relative to the stated root path.
-        """
-        if pyx_filename is None:
-            assert(c_filename.endswith('.c'))
-            pyx_filename = c_filename[:-2] + '.pyx'
-        self.c_filename = c_filename
-        self.pyx_filename = pyx_filename
-        self.deps = [pyx_filename]
-        if deps is not None:
-            self.deps.extend(deps)
-        if root is not None:
-            self.deps = [os.path.join(root, d) for d in self.deps]
-            self.c_filename = os.path.join(root, self.c_filename)
-            self.pyx_filename = os.path.join(root, self.pyx_filename)
-            
-    def build(self, force=False):
-        """
-        Run cython (and any post-filters) to recreate the C file.  Only do
-        so if the C file does not exist or is older than the declared
-        dependencies.  Pass force=True to force build.
-        """
-        build = force or (not os.path.exists(self.c_filename))
-        if not build:
-            timestamp = os.path.getmtime(self.c_filename)
-            for dep in self.deps:
-                if not os.path.exists(dep):
-                    raise RuntimeError("Dependency '%s' not found." % dep)
-                if os.path.getmtime(dep) > timestamp:
-                    build = True
-                    break
-        if build:
-            print('Calling cython %s -> %s' % (self.pyx_filename, self.c_filename))
-            commands = ['cython %s -o %s' % (self.pyx_filename, self.c_filename),
-                        "sed -i 's/typedef npy_float64 _Complex/typedef double _Complex/; "
-                        "s/typedef npy_float32 _Complex/typedef float _Complex/' "
-                        "%s" % self.c_filename]
-            for command in commands:
-                retval = sp.call(command, shell=True)
-                if retval != 0:
-                    raise RuntimeError("Failed cython processing, command: '%s'" % command)
-
-CYTHON_TARGETS = [
-    CythonTarget('sharp.c', deps=['csharp.pxd', 'csharp.h'],
-                 root='src/sharp/')
-]
-
-
-class CythonicBuild(build_ext):
+class CustomBuild(build_ext):
     def run(self):
         # Handle the special external dependencies.
         if not os.path.exists('_deps/libsharp/libsharp/sharp.h'):
             sp.call('scripts/install_libsharp.sh', shell=True)
 
-        # Pre-construct any cython .c files that we need.
-        for target in CYTHON_TARGETS:
-            target.build()
+        # Handle cythonization to create sharp.c, etc.
+        sp.call('make -C cython/', shell=True)
 
         # Then let setuptools do its thing.
         return build_ext.run(self)
@@ -118,7 +63,7 @@ setup(
     },
     ext_modules=[
         Extension('sotools.sharp',
-                  sources=['src/sharp/sharp.c'],
+                  sources=['cython/sharp.c'],
                   libraries=['sharp','c_utils', 'fftpack'],
                   library_dirs=['_deps/libsharp/auto/lib'],
                   include_dirs=[np.get_include()],
@@ -139,5 +84,5 @@ setup(
     url='https://github.com/simonsobs/sotools',
     version='0.1.0',
     zip_safe=False,
-    cmdclass={'build_ext': CythonicBuild}
+    cmdclass={'build_ext': CustomBuild}
 )
