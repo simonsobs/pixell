5238c6a6f6e2444172cf21c327fc92a1bfe40d2b
Mathew Madhavacheril <mathewsyriac@gmail.com>
2018-09-23 23:29:43 -0400

    This implements a framework for pixel-level tests of many functions including submap, read_map and postage_stamp. The framework involves a developer generating results from tests specified in tests/tests.yml, and committing these as pkl files to tests/data. This result upload should only be done very rarely. Subsequently, py.test will then re-run these tests and compare against the uploaded results. This commit also adds a bunch of usage docs.

---
+++ tests/pixel_test_generator.py
@@ -0,0 +1,13 @@
+import pixel_tests as ptests
+import pickle
+import os
+
+i = 0
+yaml_file = ""
+while not(os.path.isfile(yaml_file)) or i==0:
+    if i>0: print("File does not exist.")
+    yaml_file = input("Enter name of yaml file to generate test results from: ")
+    i += 1
+pkl_file = input("Enter name of pickle file to save test results to (exclude extension): ")
+results,_ = ptests.get_extraction_test_results(yaml_file)
+pickle.dump(results,open("data/%s.pkl" % pkl_file,'wb'))
