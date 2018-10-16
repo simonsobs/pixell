0f99c8e76d53a7a65f52e2107cd2c940147d38bd
Mathew Madhavacheril <mathewsyriac@gmail.com>
2018-10-16 12:51:30 -0400

    Bump version: 0.1.4 → 0.2.0

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/simonsobs/pixell',
-    version='0.1.4',
+    version='0.2.0',
     zip_safe=False,
     cmdclass={'build_ext': CustomBuild,
               'build_src': CustomSrc,
