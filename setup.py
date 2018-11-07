30065cd20a0d79a7391862f0eea51390cf52c38a
Mathew Madhavacheril <mathewsyriac@gmail.com>
2018-11-06 20:39:57 -0500

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -130,7 +130,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/simonsobs/pixell',
-    version='0.2.0',
+    version='0.3.0',
     zip_safe=False,
     cmdclass={'build_ext': CustomBuild,
               'build_src': CustomSrc,
