efbdd6f288b091fc601defad0899323231f52e57
Matthew Hasselfield <mhasself@users.noreply.github.com>
2018-11-07 17:55:20 -0500

    Use versioneer to manage code version.

--- pixell/__init__.py
+++ pixell/__init__.py
@@ -4,4 +4,8 @@
 
 __author__ = """Simons Observatory Collaboration Analysis Library Task Force"""
 __email__ = ''
-__version__ = '0.4.0'
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
