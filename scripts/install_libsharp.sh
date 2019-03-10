c3cf1a45be7c289882dc9f4645827c4a2ae2f1b5
Mathew Madhavacheril <mathewsyriac@gmail.com>
2019-03-09 22:44:15 -0500

    mac os adaptations

--- scripts/install_libsharp.sh
+++ scripts/install_libsharp.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 DEPDIR=_deps
 [ -e $DEPDIR ] || mkdir $DEPDIR
 cd $DEPDIR
@@ -8,9 +8,22 @@ aclocal
 if [ $? -eq 0 ]; then
     echo Found automake.
 else
-    echo WARNING: automake not found. Please install this or libsharp will not be installed correctly.
-    exit 127
-fi
+	if [ "$(uname)" == "Darwin" ]; then
+		echo WARNING: automake not found. Since this looks like Mac OS, attempting to install it.
+		brew install autoconf
+		aclocal
+	elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+		echo WARNING: automake not found. Since this looks like Linux, attempting to load its module.
+		module load autotools
+		aclocal
+	fi
+	if [ $? -eq 0 ]; then
+		echo Found automake.
+	else
+		echo WARNING: automake not found. Please install this or libsharp will not be installed correctly.
+		exit 127
+	fi
+	
 autoconf
 ./configure --enable-pic
 make
