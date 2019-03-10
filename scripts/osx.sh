9786d3e13179932449f4d4471fe0e0e9faca41fa
Mathew Madhavacheril <mathewsyriac@gmail.com>
2019-03-10 00:04:21 -0500

    more mac os adaptations

---
+++ scripts/osx.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]; then
+	if (gcc --version | grep clang); then
+
+		brew --version
+		if [ $? -eq 0 ]; then
+			echo
+		else
+			echo "NOTE: You might need to enter your user password since we are going to attempt to install homebrew."
+			ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
+		fi
+		
+		# We need to install gcc and gfortran
+		brew reinstall gcc --without-multilib
+	fi
+
+fi
