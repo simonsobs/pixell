aa32f18134984299aa9a5cdba9df43badaa576ec
Mathew Madhavacheril <mathewsyriac@gmail.com>
2019-03-10 17:03:24 -0400

    more mac os adaptations

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ compile_opts = {
     'extra_compile_args': ['-std=c99','-fopenmp', '-Wno-strict-aliasing'],
     'extra_f90_compile_args': ['-fopenmp', '-Wno-conversion', '-Wno-tabs'],
     'f2py_options': ['skip:', 'map_border', 'calc_weights', ':'],
-    # 'extra_link_args': ['-fopenmp','-lgomp']
+    'extra_link_args': ['-lgomp']
     }
 
 fcflags = os.getenv('FCFLAGS')
