73992aeb7500437bbe71cbf30762f2342dc3ec11
Mathew Madhavacheril <mathewsyriac@gmail.com>
2019-03-12 10:41:03 -0400

    more mac os adaptations

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ elif sys.platform == 'darwin':
         rpath = '/usr/local/opt/gcc/lib/gcc/' + gccpath[-1].split(os.sep)[-1][-1] + '/'
     else:
         raise('Cannot find gcc 4.x, 5.x, 6.x, 7.x, or 8.x in /usr/local/bin. pixell requires gcc to be installed - easily done through the Homebrew package manager (http://brew.sh). Note: gcc with OpenMP support is required.')
-    compile_opts['extra_link_args'] = ['-fopenmp']
+    compile_opts['extra_link_args'] = ['-fopenmp', '-Wl,-rpath,' + rpath]
 # Linux
 elif sys.platform == 'linux':
     compile_opts['extra_link_args'] = ['-fopenmp']
