c6e490b453925a4af88459bc203611900db4fd7f
Matthew Hasselfield <mhasself@users.noreply.github.com>
2019-09-03 10:25:43 -0400

    Fix indentation.  Whitespace-only change.

--- tests/test_pixell.py
+++ tests/test_pixell.py
@@ -48,93 +48,93 @@ def get_lens_result(res=1.,lmax=400,dtype=np.float64,seed=1):
     return lensed
 
 class PixelTests(unittest.TestCase):
-  def test_offset(self):
-    obs_pos,grad,raw_pos = get_offset_result(1.)
-    obs_pos0 = enmap.read_map(DATA_PREFIX+"MM_offset_obs_pos_042219.fits")
-    grad0 = enmap.read_map(DATA_PREFIX+"MM_offset_grad_042219.fits")
-    raw_pos0 = enmap.read_map(DATA_PREFIX+"MM_offset_raw_pos_042219.fits")
-    assert np.all(np.isclose(obs_pos,obs_pos0))
-    assert np.all(np.isclose(raw_pos,raw_pos0))
-    assert np.all(np.isclose(grad,grad0))
-    assert wcsutils.equal(grad.wcs,grad0.wcs)
-    assert wcsutils.equal(obs_pos.wcs,obs_pos0.wcs)
-    assert wcsutils.equal(raw_pos.wcs,raw_pos0.wcs)
+    def test_offset(self):
+        obs_pos,grad,raw_pos = get_offset_result(1.)
+        obs_pos0 = enmap.read_map(DATA_PREFIX+"MM_offset_obs_pos_042219.fits")
+        grad0 = enmap.read_map(DATA_PREFIX+"MM_offset_grad_042219.fits")
+        raw_pos0 = enmap.read_map(DATA_PREFIX+"MM_offset_raw_pos_042219.fits")
+        assert np.all(np.isclose(obs_pos,obs_pos0))
+        assert np.all(np.isclose(raw_pos,raw_pos0))
+        assert np.all(np.isclose(grad,grad0))
+        assert wcsutils.equal(grad.wcs,grad0.wcs)
+        assert wcsutils.equal(obs_pos.wcs,obs_pos0.wcs)
+        assert wcsutils.equal(raw_pos.wcs,raw_pos0.wcs)
 
-  def test_lensing(self):
-    lensed,unlensed = get_lens_result(1.,400,np.float64)
-    lensed0 = enmap.read_map(DATA_PREFIX+"MM_lensed_042219.fits")
-    unlensed0 = enmap.read_map(DATA_PREFIX+"MM_unlensed_042219.fits")
-    y,x = lensed0.posmap()
-    assert np.all(np.isclose(lensed,lensed0)) 
-    assert np.all(np.isclose(unlensed,unlensed0))
-    assert wcsutils.equal(lensed.wcs,lensed0.wcs)
-    assert wcsutils.equal(unlensed.wcs,unlensed0.wcs)
-    assert wcsutils.equal(unlensed.wcs,lensed.wcs)
+    def test_lensing(self):
+        lensed,unlensed = get_lens_result(1.,400,np.float64)
+        lensed0 = enmap.read_map(DATA_PREFIX+"MM_lensed_042219.fits")
+        unlensed0 = enmap.read_map(DATA_PREFIX+"MM_unlensed_042219.fits")
+        y,x = lensed0.posmap()
+        assert np.all(np.isclose(lensed,lensed0))
+        assert np.all(np.isclose(unlensed,unlensed0))
+        assert wcsutils.equal(lensed.wcs,lensed0.wcs)
+        assert wcsutils.equal(unlensed.wcs,unlensed0.wcs)
+        assert wcsutils.equal(unlensed.wcs,lensed.wcs)
     
-  def test_enplot(self):
-    print("Testing enplot...")
-    shape,wcs = enmap.geometry(pos=(0,0),shape=(3,100,100),res=0.01)
-    a = enmap.ones(shape,wcs)
-    p = enplot.get_plots(a)
+    def test_enplot(self):
+        print("Testing enplot...")
+        shape,wcs = enmap.geometry(pos=(0,0),shape=(3,100,100),res=0.01)
+        a = enmap.ones(shape,wcs)
+        p = enplot.get_plots(a)
 
-  def test_fft(self):
-    # Tests that ifft(ifft(imap))==imap, i.e. default normalizations are consistent
-    shape,wcs = enmap.geometry(pos=(0,0),shape=(3,100,100),res=0.01)
-    imap = enmap.enmap(np.random.random(shape),wcs)
-    assert np.all(np.isclose(imap,enmap.ifft(enmap.fft(imap,normalize='phy'),normalize='phy').real))
-    assert np.all(np.isclose(imap,enmap.ifft(enmap.fft(imap)).real))
+    def test_fft(self):
+        # Tests that ifft(ifft(imap))==imap, i.e. default normalizations are consistent
+        shape,wcs = enmap.geometry(pos=(0,0),shape=(3,100,100),res=0.01)
+        imap = enmap.enmap(np.random.random(shape),wcs)
+        assert np.all(np.isclose(imap,enmap.ifft(enmap.fft(imap,normalize='phy'),normalize='phy').real))
+        assert np.all(np.isclose(imap,enmap.ifft(enmap.fft(imap)).real))
 
-  def test_extract(self):
-    # Tests that extraction is sensible
-    shape,wcs = enmap.geometry(pos=(0,0),shape=(500,500),res=0.01)
-    imap = enmap.enmap(np.random.random(shape),wcs)
-    smap = imap[200:300,200:300]
-    sshape,swcs = smap.shape,smap.wcs
-    smap2 = enmap.extract(imap,sshape,swcs)
-    pixbox = enmap.pixbox_of(imap.wcs,sshape,swcs)
-    # Do write and read test
-    filename = "temporary_extract_map.fits" # NOT THREAD SAFE
-    enmap.write_map(filename,imap)
-    smap3 = enmap.read_map(filename,pixbox=pixbox)
-    os.remove(filename)
-    assert np.all(np.isclose(smap,smap2))
-    assert np.all(np.isclose(smap,smap3))
-    assert wcsutils.equal(smap.wcs,smap2.wcs)
-    assert wcsutils.equal(smap.wcs,smap3.wcs)
-    
-  def test_fullsky_geometry(self):
-    # Tests whether number of pixels and area of a full-sky 0.5 arcminute resolution map are correct
-    print("Testing full sky geometry...")
-    test_res_arcmin = 0.5
-    shape,wcs = enmap.fullsky_geometry(res=np.deg2rad(test_res_arcmin/60.),proj='car')
-    assert shape[0]==21601 and shape[1]==43200
-    assert 50000 < (enmap.area(shape,wcs)*(180./np.pi)**2.) < 51000
+    def test_extract(self):
+        # Tests that extraction is sensible
+        shape,wcs = enmap.geometry(pos=(0,0),shape=(500,500),res=0.01)
+        imap = enmap.enmap(np.random.random(shape),wcs)
+        smap = imap[200:300,200:300]
+        sshape,swcs = smap.shape,smap.wcs
+        smap2 = enmap.extract(imap,sshape,swcs)
+        pixbox = enmap.pixbox_of(imap.wcs,sshape,swcs)
+        # Do write and read test
+        filename = "temporary_extract_map.fits" # NOT THREAD SAFE
+        enmap.write_map(filename,imap)
+        smap3 = enmap.read_map(filename,pixbox=pixbox)
+        os.remove(filename)
+        assert np.all(np.isclose(smap,smap2))
+        assert np.all(np.isclose(smap,smap3))
+        assert wcsutils.equal(smap.wcs,smap2.wcs)
+        assert wcsutils.equal(smap.wcs,smap3.wcs)
 
-  def test_pixels(self):
-    """Runs reference pixel and mean-square comparisons on extracts from randomly generated
-    maps"""
-    print("Testing reference pixels...")
-    results,rname = ptests.get_extraction_test_results(TEST_DIR+"/tests.yml")
-    cresults = pickle.load(open(DATA_PREFIX+"%s.pkl" % rname,'rb'))
-    assert sorted(results.keys())==sorted(cresults.keys())
-    for g in results.keys():
-        assert sorted(results[g].keys())==sorted(cresults[g].keys())
-        for s in results[g].keys():
-            assert sorted(results[g][s].keys())==sorted(cresults[g][s].keys())
-            for e in results[g][s].keys():
-                assert np.all(np.isclose(results[g][s][e],cresults[g][s][e]))
+    def test_fullsky_geometry(self):
+        # Tests whether number of pixels and area of a full-sky 0.5 arcminute resolution map are correct
+        print("Testing full sky geometry...")
+        test_res_arcmin = 0.5
+        shape,wcs = enmap.fullsky_geometry(res=np.deg2rad(test_res_arcmin/60.),proj='car')
+        assert shape[0]==21601 and shape[1]==43200
+        assert 50000 < (enmap.area(shape,wcs)*(180./np.pi)**2.) < 51000
+
+    def test_pixels(self):
+        """Runs reference pixel and mean-square comparisons on extracts from randomly generated
+        maps"""
+        print("Testing reference pixels...")
+        results,rname = ptests.get_extraction_test_results(TEST_DIR+"/tests.yml")
+        cresults = pickle.load(open(DATA_PREFIX+"%s.pkl" % rname,'rb'))
+        assert sorted(results.keys())==sorted(cresults.keys())
+        for g in results.keys():
+            assert sorted(results[g].keys())==sorted(cresults[g].keys())
+            for s in results[g].keys():
+                assert sorted(results[g][s].keys())==sorted(cresults[g][s].keys())
+                for e in results[g][s].keys():
+                    assert np.all(np.isclose(results[g][s][e],cresults[g][s][e]))
 
 
-  def test_sim_slice(self):
-    ps = powspec.read_spectrum(DATA_PREFIX+"test_scalCls.dat")[:1,:1]
-    test_res_arcmin = 10.0
-    lmax = 2000
-    fact = 2.
-    shape,wcs = enmap.fullsky_geometry(res=np.deg2rad(test_res_arcmin/60.),proj='car')
-    omap = curvedsky.rand_map(shape, wcs, ps,lmax=lmax)
-    ofunc = lambda ishape,iwcs: fact*enmap.extract(omap,ishape,iwcs)
-    nmap = reproject.populate(shape,wcs,ofunc,maxpixy = 400,maxpixx = 400)
-    assert np.all(np.isclose(nmap/omap,2.))
+    def test_sim_slice(self):
+        ps = powspec.read_spectrum(DATA_PREFIX+"test_scalCls.dat")[:1,:1]
+        test_res_arcmin = 10.0
+        lmax = 2000
+        fact = 2.
+        shape,wcs = enmap.fullsky_geometry(res=np.deg2rad(test_res_arcmin/60.),proj='car')
+        omap = curvedsky.rand_map(shape, wcs, ps,lmax=lmax)
+        ofunc = lambda ishape,iwcs: fact*enmap.extract(omap,ishape,iwcs)
+        nmap = reproject.populate(shape,wcs,ofunc,maxpixy = 400,maxpixx = 400)
+        assert np.all(np.isclose(nmap/omap,2.))
 
 
 if __name__ == '__main__':
