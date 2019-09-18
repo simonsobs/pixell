173c57050425c1f696ecd6a95b6aad090850345b
Mathew Madhavacheril <mathewsyriac@gmail.com>
2019-09-18 16:40:55 -0400

    re-generated lensing and pixell tests

--- scripts/generate_tests/generate_test_lens.py
+++ scripts/generate_tests/generate_test_lens.py
@@ -5,11 +5,13 @@ import os
 import numpy as np
 from pixell import enmap
 
+version = sys.argv[1]
+
 obs_pos,grad,raw_pos = ptests.get_offset_result(1.,np.float64)
-enmap.write_map("MM_offset_obs_pos_042219.fits",obs_pos)
-enmap.write_map("MM_offset_grad_042219.fits",grad)
-enmap.write_map("MM_offset_raw_pos_042219.fits",raw_pos)
+enmap.write_map("MM_offset_obs_pos_%s.fits" % version,obs_pos)
+enmap.write_map("MM_offset_grad_%s.fits"  % version,grad)
+enmap.write_map("MM_offset_raw_pos_%s.fits"  % version,raw_pos)
 
 lensed,unlensed = ptests.get_lens_result(1.,400,np.float64)
-enmap.write_map("MM_lensed_042219.fits",lensed)
-enmap.write_map("MM_unlensed_042219.fits",unlensed)
+enmap.write_map("MM_lensed_%s.fits"  % version,lensed)
+enmap.write_map("MM_unlensed_%s.fits"  % version,unlensed)
