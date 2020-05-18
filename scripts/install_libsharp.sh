b26f2bc2df19130db41a434e3fc1a69bc5774281
Mathew Madhavacheril <mathewsyriac@gmail.com>
2020-05-17 21:40:10 -0400

    test configuration for osx

--- scripts/install_libsharp.sh
+++ scripts/install_libsharp.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 DEPDIR=_deps
 [ -e $DEPDIR ] || mkdir $DEPDIR
 cd $DEPDIR
@@ -8,10 +9,38 @@ aclocal
 if [ $? -eq 0 ]; then
     echo Found automake.
 else
-    echo ERROR: automake not found. Please install this or libsharp will not be installed correctly.
-    exit 127
+	if [ "$(uname)" == "Darwin" ]; then
+		echo WARNING: automake not found. Since this looks like Mac OS, attempting to install it.
+		brew install autoconf automake
+		if [ $? -eq 0 ]; then
+			echo
+		else
+			echo "NOTE: You might need to enter your user password since we are going to attempt to install homebrew."
+			ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
+			brew install autoconf automake
+		fi
+		aclocal
+	elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+		echo WARNING: automake not found. Since this looks like Linux, attempting to load its module.
+		module load autotools
+		aclocal
+	fi
+	if [ $? -eq 0 ]; then
+		echo Found automake.
+	else
+		echo WARNING: automake not found. Please install this or libsharp will not be installed correctly.
+		exit 127
+	fi
 fi
 autoconf
 ./configure --enable-pic
+cat config.log
 make
+if [ $? -eq 0 ]; then
+    echo Successfully installed libsharp.
+	touch success.txt
+else
+	echo ERROR: Libsharp did not install correctly.
+	exit 127
+fi
 rm -rf python/
