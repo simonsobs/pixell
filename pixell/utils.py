20092edb9f5ce706d10316b422389a7138084334
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2022-03-26 19:09:01 +0100

    Working on C-version of source simulation

--- pixell/utils.py
+++ pixell/utils.py
@@ -2451,7 +2451,91 @@ def tform_to_profile(bl, theta, normalize=False):
 # Compatibility
 beam_transform_to_profile = tform_to_profile
 
-def profile_to_tform_hankel(profile_fun, lmin=0.01, lmax=1e6, n=512, pad=256):
+class RadialFourierTransform:
+	def __init__(self, lrange=None, rrange=None, n=512, pad=256):
+		"""Construct an object for transforming between radially
+		symmetric profiles in real-space and fourier space using a
+		fast Hankel transform. Aside from being fast, this is also
+		good for representing both cuspy and very extended profiles
+		due to the logarithmically spaced sample points the fast
+		Hankel transform uses. A cost of this is that the user can't
+		freely choose the sample points. Instead one passes the
+		multipole range or radial range of interest as well as the
+		number of points to use.
+
+		The function currently assumes two dimensions with flat geometry.
+		That means the function is only approximate for spherical
+		geometries, and will only be accurate up to a few degrees
+		in these cases.
+
+		Arguments:
+		* lrange = [lmin, lmax]: The multipole range to use. Defaults
+		  to [0.01, 1e6] if no rrange is given.
+		* rrange = [rmin, rmax]: The radius range to use if lrange is
+			not specified, in radians. Example values: [1e-7,10].
+			Since we don't use spherical geometry r is not limited to 2 pi.
+		* n: The number of logarithmically equi-spaced points to use
+			in the given range. Default: 512. The Hankel transform usually
+			doesn't need many points for good accuracy, and can suffer if
+			too many points are used.
+		* pad: How many extra points to pad by on each side of the range.
+		  Padding is useful to get good accuracy in a Hankel transform.
+		  The transforms this function does will return padded output,
+			which can be unpadded using the unpad method. Default: 256
+		"""
+		if lrange is None and rrange is None: lrange = [0.1, 1e7]
+		if lrange is None: lrange = [1/rrange[1], 1/rrange[0]]
+		logl1, logl2 = np.log(lrange)
+		logl0        = (logl2+logl1)/2
+		self.dlog    = (logl2-logl1)/n
+		i0           = (n+1)/2+pad
+		self.l       = np.exp(logl0 + (np.arange(1,n+2*pad+1)-i0)*self.dlog)
+		self.r       = 1/self.l[::-1]
+		self.pad     = pad
+	def real2harm(self, rprof):
+		"""Perform a forward (real -> harmonic) transform, taking us from the
+		provided real-space radial profile rprof(r) to a harmonic-space profile
+		lprof(l). rprof can take two forms:
+		1. A function rprof(r) that can be called to evalute the profile at
+		   arbitrary points.
+		2. An array rprof[self.r] that provides the profile evaluated at the
+		   points given by this object's .r member.
+		The transform is done along the last axis of the profile.
+		Returns lprof[self.l]. This includes padding, which can be removed
+		using self.unpad"""
+		import scipy.fft
+		try: rprof = rprof(self.r)
+		except TypeError: pass
+		lprof = 2*np.pi*scipy.fft.fht(rprof*self.r, self.dlog, 0)/self.l
+		return lprof
+	def harm2real(self, lprof):
+		"""Perform a backward (harmonic -> real) transform, taking us from the
+		provided harmonic-space radial profile lprof(l) to a real-space profile
+		rprof(r). lprof can take two forms:
+		1. A function lprof(l) that can be called to evalute the profile at
+		   arbitrary points.
+		2. An array lprof[self.l] that provides the profile evaluated at the
+		   points given by this object's .l member.
+		The transform is done along the last axis of the profile.
+		Returns rprof[self.r]. This includes padding, which can be removed
+		using self.unpad"""
+		import scipy.fft
+		try: lprof = lprof(self.l)
+		except TypeError: pass
+		rprof = scipy.fft.ifht(lprof/(2*np.pi)*self.l, self.dlog, 0)/self.r
+		return rprof
+	def unpad(self, *arrs):
+		"""Remove the padding from arrays used by this object. The
+		values in the padded areas of the output of the transform have
+		unreliable values, but they're not cropped automatically to
+		allow for round-trip transforms. Example:
+			r = unpad(r_padded)
+			r, l, vals = unpad(r_padded, l_padded, vals_padded)"""
+		if self.pad == 0: res = arrs
+		else: res = tuple([arr[...,self.pad:-self.pad] for arr in arrs])
+		return res[0] if len(arrs) == 1 else res
+
+def profile_to_tform_hankel(profile_fun, lmin=0.1, lmax=1e7, n=512, pad=256):
 	"""Transform a radial profile given by the function profile_fun(r) to
 	sperical harmonic coefficients b(l) using a Hankel transform. This approach
 	is good at handling cuspy distributions due to using logarithmically spaced
@@ -2460,25 +2544,9 @@ def profile_to_tform_hankel(profile_fun, lmin=0.01, lmax=1e6, n=512, pad=256):
 	the results. Note that unlike other similar functions in this module and
 	the curvedsky module, this function uses the flat sky approximation, so
 	it should only be used for profiles up to a few degrees in size."""
-	import scipy.fft
-	# Prepare the real-space and fourier-space sample points, which are
-	# logarithmically spaced
-	loglmin = np.log10(lmin)
-	loglmax = np.log10(lmax)
-	logl0   = (loglmax+loglmin)/2
-	dlogl   = (loglmax-loglmin)/n
-	dln     = dlogl*np.log(10)
-	i0      = (n+1)/2+pad
-	l       = 10**(logl0 + (np.arange(1,n+2*pad+1)-i0)*dlogl)
-	r       = 1/l[::-1]
-	# Evaluate our profile
-	rprof   = profile_fun(r)
-	lprof   = 2*np.pi*scipy.fft.fht(rprof*r, dln, 0)/l
-	# Crop off the padding
-	l       = l[pad:-pad]
-	lprof   = lprof[pad:-pad]
-	return l, lprof
-
+	rht   = RadialFourierTransform(lrange=[lmin,lmax], n=n, pad=pad)
+	lprof = rht.real2harm(profile_fun)
+	return rht.unpad(rht.l, lprof)
 
 def fix_dtype_mpi4py(dtype):
 	"""Work around mpi4py bug, where it refuses to accept dtypes with endian info"""
