6a8247df9015e279c779cd2cb7ae6e5a4d6dc6df
Josh Borrow <josh@joshborrow.com>
2023-10-16 13:54:25 -0400

    Rename fcompiler

--- setup.py
+++ setup.py
@@ -174,7 +174,7 @@ def prebuild():
 
 
 class CustomBuild(build_ext):
-    f77_compiler: str = os.environ["FC"]
+    fcompiler: str = os.environ["FC"]
     def run(self):
         print("Running build...")
         prebuild()
