2a2830898d8655c94349bf7c3d663329de9bf8a2
Josh Borrow <josh@joshborrow.com>
2024-07-02 09:33:15 -0400

    Allow for dynamic versioning with versioneer

--- pixell/_version.py
+++ pixell/_version.py
@@ -518,3 +518,7 @@ def get_versions():
     return {"version": "0+unknown", "full-revisionid": None,
             "dirty": None,
             "error": "unable to compute version", "date": None}
+
+
+if __name__ == "__main__":
+    print(get_versions()["version"])
