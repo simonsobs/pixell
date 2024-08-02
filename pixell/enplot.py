2c18126383048aa7f01501c744e6ce3ca3205100
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2024-08-02 14:48:24 +0200

    Fix unit test failure in enplot, probably also caused by a numpy 2.0 change

--- pixell/enplot.py
+++ pixell/enplot.py
@@ -915,7 +915,7 @@ def draw_ellipse(image, bounds, width=1, outline='white', antialias=1):
 		draw.ellipse([a[0],a[1],b[0],b[1]], fill=fill)
 	# downsample the mask using PIL.Image.LANCZOS 
 	# (a high-quality downsampling filter).
-	mask = mask.resize(esize, PIL.Image.LANCZOS)
+	mask = mask.resize(tuple(esize), PIL.Image.LANCZOS)
 	# paste outline color to input image through the mask
 	image.paste(outline, tuple(bounds[:2]-width), mask=mask)
 
